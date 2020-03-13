d0ae9fa87741fac521651b6a420a38f06a17436a
heshan <40200190@qq.com>
2020-03-13 16:07:50 +0800

    add index page

--- MyScripts/app.py
+++ MyScripts/app.py
@@ -1,14 +1,14 @@
-from flask import Flask, url_for,request
+from flask import Flask, url_for,request,render_template
 from markupsafe import escape
 
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return 'Welcome to my Wachlist!'
 
-# @app.route('/')
-# def index():
-#     return 'index'
+
+@app.route('/home')
+@app.route('/index')
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/login')
 def login():
@@ -19,15 +19,22 @@ def profile(username):
     return '{}\'s profile'.format(escape(username))
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         return do_the_login()
-#     else:
-#         return show_the_login_form()
+name = 'Grey Li'
+movies = [
+{'title': 'My Neighbor Totoro', 'year': '1988'},
+{'title': 'Dead Poets Society', 'year': '1989'},
+{'title': 'A Perfect World', 'year': '1993'},
+{'title': 'Leon', 'year': '1994'},
+{'title': 'Mahjong', 'year': '1996'},
+{'title': 'Swallowtail Butterfly', 'year': '1996'},
+{'title': 'King of Comedy', 'year': '1999'},
+{'title': 'Devils on the Doorstep', 'year': '1999'},
+{'title': 'WALL-E', 'year': '2008'},
+{'title': 'The Pork of Music', 'year': '2012'},
+]
 
 with app.test_request_context():
-    print(url_for('hello'))
+    print(url_for('index'))
     print(url_for('login'))
     print(url_for('login', next='/'))
     print(url_for('profile', username='John Doe'))
